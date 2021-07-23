94673d572db3e496d6a8a496369b70d8d92a9121
Fabricio Anibal <fabricio_anibal@hotmail.com>
2021-07-22 23:11:32 -0300

    Adicionado o script do killer e alguns assets

---
+++ Assets/Scripts/KillerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Floor"))
+        {
+            rb.simulated = false;
+        }
+    }
+}
