bd78bc5cfb6c516cb38c9e01aa25637a1ab50c9e
Fabricio Anibal <fabricio_anibal@hotmail.com>
2021-07-24 00:44:30 -0300

    Reposição de munição após paralização do disparo anterior

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.relativeVelocity.magnitude);
+    }
+}
--- Assets/Scripts/KunController.cs
+++ Assets/Scripts/KunController.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class KunController : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    Rigidbody2D rb;
+
+    SpringJoint2D sj;
+
+    public GameObject hook;
 
     public float releaseTime = 0f;
 
     private bool isPressed = false;
 
+    private bool released = false;
+
     private void OnMouseDown()
     {
         isPressed = true;
         rb.isKinematic = true;
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        sj = GetComponent<SpringJoint2D>();
     }
 
     private void OnMouseUp()
@@ -30,14 +43,35 @@ public class KunController : MonoBehaviour
         {
             rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
+
+        if(rb.velocity.magnitude == 0 && released)
+        {
+            //Debug.Log(rb.velocity.magnitude);
+
+            sj.enabled = true;
+
+            transform.position = hook.transform.position;
+        }
+        
     }
 
     IEnumerator Release()
     {
         yield return new WaitForSeconds(releaseTime);
 
-        GetComponent<SpringJoint2D>().enabled = false;
+        sj.enabled = false;
 
         rb.velocity *= 2;
+
+        released = true;
+    }
+
+    public void ResetLaunch()
+    {
+        //new WaitForSeconds(1);
+
+        transform.position = sj.gameObject.transform.position;
+
+        sj.enabled = true;
     }
 }
