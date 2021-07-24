966c8cd6f6e6c525f85e27d5827819482f47eb3c
Fabricio Anibal <fabricio_anibal@hotmail.com>
2021-07-23 23:37:45 -0300

    Implementado o lançamento da munição.

---
+++ Assets/Scripts/KunController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KunController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float releaseTime = .15f;
+
+    private bool isPressed = false;
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+    private void Update()
+    {
+        if(isPressed)
+        {
+            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+
+        rb.velocity *= 2;
+    }
+}
